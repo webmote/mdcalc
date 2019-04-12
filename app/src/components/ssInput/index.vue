2ae6a7d16af11523856211e5c0e191bffe6d8ef1
冯朋斌 <fengpb@1mdata.com>
2019-04-13 02:25:40 +0800

    fix

--- app/src/components/ssInput/index.vue
+++ app/src/components/ssInput/index.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
-    <div class="input-options" v-bind:class="{wrap: ss.options&&ss.options.length>3}" v-if="ss.options&&ss.options.length>0">
+    <!-- v-bind:class="{wrap: ss.options&&ss.options.length>3}" -->
+    <div class="input-options" v-if="ss.options&&ss.options.length>0">
       <button tabindex="-1" v-bind:value="o.key" v-bind:class="{active: value == o.key}" v-for="o in ss.options" v-bind:key="o.key" v-on="optionListeners">{{o.name}}</button>
     </div>
     <div class="input" v-else>
       <input v-bind="$attrs" v-bind:tabindex="index" v-bind:value="value" type="text" v-on="inputListeners">
-      <button tabindex="-1" v-bind:value="unit.unit" v-bind:unit="unit.unit" v-if="unit.unit" v-on="unitListeners">{{ unit.units && unit.units.length>0 ? '⇌&nbsp;'+unit.unit:unit.unit}}</button>
+      <button class="button" tabindex="-1" v-bind:value="unit.unit" v-bind:unit="unit.unit" v-if="unit.unit" v-on="unitListeners">{{unit.unit}}
+        <svg v-if="unit.units && unit.units.length>0" fill="#1bb193" style="width:13px;vertical-align:middle;margin-left:5px;" viewBox="0 0 13 13" data-reactid="278"><path d="M12,6H1C0.6,6,0.3,5.8,0.1,5.5s-0.2-0.7,0-1l2.3-4c0.3-0.5,1-0.6,1.4-0.4C4.3,0.4,4.5,1,4.2,1.5L2.7,4H12c0.6,0,1,0.4,1,1 S12.6,6,12,6z" data-reactid="279"></path><path d="M1,7h11c0.4,0,0.7,0.2,0.9,0.5s0.2,0.7,0,1l-2.3,4c-0.3,0.5-0.9,0.6-1.4,0.4c-0.5-0.3-0.6-0.9-0.4-1.4L10.3,9H1   C0.4,9,0,8.6,0,8S0.4,7,1,7z" data-reactid="280"></path></svg>
+      </button>
     </div>
   </div>
 </template>
@@ -71,6 +74,12 @@ export default {
       return Object.assign({}, this.$listeners, {
         click: function (event) {
           var unit = event.target.value
+          if (event.target.nodeName === 'svg') {
+            unit = event.target.parentNode.value
+          }
+          else if (event.target.nodeName === 'path') {
+            unit = event.target.parentNode.parentNode.value
+          }
           if (vm.unit.units && vm.unit.units.length > 1) {
             var unitSize = vm.unit.units.length
             var index = _.indexOf(vm.unit.units, unit)
@@ -99,7 +108,7 @@ export default {
   display: flex;
 }
 .input {
-  border: 1px solid #1BB193;
+  border: 1px solid #ccc;
 }
 .input input {
   /* width: 20em; */
@@ -111,8 +120,9 @@ export default {
 .input button {
   flex: 0 0 auto;
   border: none;
-  background-color: #1BB193;
-  color: white;
+  background-color: #f0f0f0;
+  color: #323232;
+  border-left: 1px solid #ccc;
 }
 .input-options {
   flex-wrap: wrap;
@@ -124,6 +134,7 @@ export default {
   background-color: #f0f0f0;
   color: #323232;
   margin-right: -1px;
+  cursor: pointer;
 }
 .input-options button:last-child {
   margin-right: 0;
