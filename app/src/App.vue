af512227bd0247d509bbc645eb8b349b425f873a
冯朋斌 <fengpb@1mdata.com>
2019-04-13 00:36:47 +0800

    nav

--- app/src/App.vue
+++ app/src/App.vue
@@ -5,17 +5,52 @@
         <a href="/" class="brand">
           <img src="./assets/logo.png" style="height: 5em; width: auto;">
         </a>
-        <router-link to="/s">About</router-link>
+        <div class="search-wrap">
+          <div class="search-input">
+            <button>q</button><input v-model="keyword" v-on="inputListeners" type="text" placeholder="Search QT or EKG"><button v-on:click="closeSearch" v-show="isShowSearch" class="close">x</button>
+          </div>
+        </div>
+        <div class="menus-wrap"></div>
       </nav>
     </section>
-    <router-view/>
+    <div class="search-results-wrap" v-show="isShowSearch"></div>
+    <router-view v-show="!isShowSearch"/>
   </div>
 </template>
 
 <script>
 import 'normalize.css'
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      isShowSearch: false,
+      keyword: null
+    }
+  },
+  computed: {
+    inputListeners: function () {
+      var vm = this
+      return Object.assign({}, this.$listeners, {
+        input: function (event) {
+          vm.$emit('input', event.target.value)
+          vm.search(event.target.value)
+        },
+        focus: function (event) {
+          vm.isShowSearch = true
+        }
+      })
+    }
+  },
+  methods: {
+    closeSearch: function (event) {
+      this.isShowSearch = false
+      this.keyword = null
+    },
+    search: function (keyword) {
+      console.log(keyword)
+    }
+  }
 }
 </script>
 
@@ -30,6 +65,48 @@ export default {
 }
 nav {
   height: 5em;
-  background-color: #2c3e50;
+  border-top: solid .2em #1BB193;
+  border-bottom: solid 1px #f0f0f0;
+  display: flex;
+}
+.brand {
+  flex: 1 1 20%
+}
+.search-wrap {
+  display: inline-block;
+  flex: 1 1 60%;
+}
+.search-input {
+  height: 3em;
+  border: 1px solid #1BB193;
+  margin-top: .8em;
+  display: flex;
+}
+.search-input * {
+  vertical-align: middle;
+}
+.search-input button {
+  height: 100%;
+  width: 3em;
+  border: none;
+  background-color: #F5BC17;
+  color: white;
+  flex-shrink: 0;
+}
+.search-input button.close {
+  margin-right: .5em;
+  margin-top: .55em;
+  height: 2em;
+  width: 2em;
+  border-radius: 1em;
+}
+.search-input input {
+  border: none;
+  font-size: 1.6em;
+  flex: auto;
+  padding-left: .4em;
+}
+.menus-wrap {
+  flex: 1 1 20%;
 }
 </style>
